df3afad09e6fc4438e4ed228ea8d8e20d5f40be2
TheDeadlyPianist <duane221@yahoo.co.uk>
2017-07-20 13:28:46 +0100

    Push CD

--- src/main/java/persistance/CD.java
+++ src/main/java/persistance/CD.java
@@ -1,11 +1,49 @@
 package persistance;
 
-/**
- * Created by Administrator on 20/07/2017.
+/*
+ * Created by Eric
+ * Edited by Duane
  */
 
 import javax.persistence.Entity;
 
 @Entity
 public class CD {
+    private int iD;
+    private String name;
+    private String artist;
+    private String genre;
+    private String year;
+
+    void setID (int input) {
+        iD = input;
+    }
+    int getID () {
+        return iD;
+    }
+    void setName (String input) {
+        name = input;
+    }
+    String getName () {
+        return name;
+    }
+    void setArtist (String input) {
+        artist = input;
+    }
+    String getArtist () {
+        return artist;
+    }
+    void setGenre (String input) {
+        genre = input;
+    }
+    String getGenre () {
+        return genre;
+    }
+    void setYear (String input) {
+        year = input;
+    }
+    String getYear () {
+        return year;
+    }
+
 }
